9926ace37e84dda7a9ab4f1f0f708a32f069e327
Nils Urbach <ndu01u@gmail.com>
2023-10-05 11:45:53 +0200

    fixed problems in consts.py

--- tests/consts.py
+++ tests/consts.py
@@ -1,22 +1,22 @@
-import sys
 from pathlib import Path
 from json import dumps
-sys.path.append("D:\Desktop\jsonExtended")
-from _core.json_extended import _indent_level
+
+indent_level = 4
+
 validJSONData = {
-    "thisastr": "IamAString",
-    "thisaint": 1234,
-    "thisafloat": -56.78,
-    "thisabool": True,
-    "thisnull": None,
-    "thisaarray": [
+    "string": "IamAString",
+    "integer": 1234,
+    "floating-point": -56.78,
+    "boolean": True,
+    "null": None,
+    "list": [
         "str",
         -23,
         4.5,
         False,
         None
     ],
-    "thisaobj": {
+    "object": {
         "first": 123,
         "second": None,
         "third": "nowIamBored"
@@ -25,68 +25,70 @@ validJSONData = {
 invalidJSONData = Path.cwd()
 
 simpleTypeKeys = [
-    "thisastr",
-    "thisaint",
-    "thisafloat",
-    "thisabool",
-    "thisnull"
+    "string",
+    "integer",
+    "floating-point",
+    "boolean",
+    "null"
 ]
-arrayKey = "thisaarray"
-objectKey = "thisaobj"
-nonTopLevelKeys = ("thisaobj", "second")
-nonTopLevelValue = validJSONData["thisaobj"]["second"]
-invalidKeys = ("thisaobj", "thisnothere")
+arrayKey = "list"
+objectKey = "object"
+nonTopLevelKeys = ("object", "second")
+nonTopLevelValue = validJSONData["object"]["second"]
+invalidKeys = ("object", "invalid")
 
 newArray = [
-     "newString",
-     100,
-     -45.78,
-     False,
-     None
+    "newString",
+    100,
+    -45.78,
+    False,
+    None
 ]
 simpleTypeKeysWithNewValues = {simpleTypeKeys[i]: newArray[i] for i in range(len(simpleTypeKeys))}
 
 validJSONStr = dumps(validJSONData)
 invalidJSONStr = validJSONStr[1:]
-indentedValidJSONStr = dumps(validJSONData, indent = _indent_level)
+indentedValidJSONStr = dumps(validJSONData, indent=indent_level)
 
-pathToValidJSON = Path.cwd() / Path(r"tests\validJSONfile.json")
-pathToInvalidJSON = Path.cwd() / Path(r"tests\invlaidJSONfile.json")
-pathToNoJSON = Path.cwd() / Path(r"tests\idontexist.json")
+pathToValidJSON = Path.cwd() / Path(r"tests\valid_json_file.json")
+pathToInvalidJSON = Path.cwd() / Path(r"tests\invalid_json_file.json")
+pathToNoJSON = Path.cwd() / Path(r"tests\dont_exist.json")
 
 mappedPythonTypes = [
-        "astring",
-        "",
-        10,
-        -10,
-        0,
-        -0,
-        123.345,
-        -3.7,
-        0.0,
-        -0.0,
-        True,
-        False,
-        None
-    ]
+    "string",
+    "",
+    10,
+    -10,
+    0,
+    -0,
+    123.345,
+    -3.7,
+    0.0,
+    -0.0,
+    True,
+    False,
+    None
+]
 
 nonEmptyList = ["str", -3, 10.5, True, None]
 emptyList = []
 
-nonEmptyDict = {"astr": "str", "aint": -3, "afloat": 10.5, "abool": True, "aNull": None}
+nonEmptyDict = {"string": "str", "int": -3, "afloat": 10.5, "bool": True, "none": None}
 emptyDict = {}
 
+
 def globalSetUp(write: bool):
-    open(file = pathToValidJSON, mode = "x").close()
-    open(file = pathToInvalidJSON, mode = "x").close()
-    if (write):
-        with open(file = pathToValidJSON, mode = "w") as fp:
-                fp.write(validJSONStr)
-        with open(file = pathToInvalidJSON, mode = "w") as fp:
-                fp.write(invalidJSONStr)
+    open(file=pathToValidJSON, mode="x").close()
+    open(file=pathToInvalidJSON, mode="x").close()
+    if write:
+        with open(file=pathToValidJSON, mode="w") as fp:
+            fp.write(validJSONStr)
+        with open(file=pathToInvalidJSON, mode="w") as fp:
+            fp.write(invalidJSONStr)
+
 
 def globalTearDown():
     pathToValidJSON.unlink()
     pathToInvalidJSON.unlink()
-    if (pathToNoJSON.exists()):
+    if pathToNoJSON.exists():
         pathToNoJSON.unlink()
